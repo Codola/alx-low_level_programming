20704c9679491be3c55533faa8626846a1dac37d
Olawale Idris <oolawaleidris@yahoo.com>
2022-09-08 18:12:07 -0700

    A C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * This program prints text to the screen
+ * using function 'puts()'
+ *
+ */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
