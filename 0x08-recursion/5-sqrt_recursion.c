e51b103b3be0feace5ae3cde79e80ac681c79f70
Codola <111078913+Codola@users.noreply.github.com>
2022-09-28 19:13:55 +0100

    Returns 1 if the input integer is a prime number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,36 @@
 #include "main.h"
-
 /**
- * _sqrt_recursion - main function
- * @n: int n
- * Return: int
+ * rCheck - checks for the root
+ * @i: integer to prime for root
+ * @n: integer supplied
+ * Return: value of root
+ */
+int rCheck(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = rCheck(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
+/**
+ * _sqrt_recursion - returns square root of n
+ * @n: number supplied
+ * Return: returns int of squareroot
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
+	int i = 0;
 
-/**
- * _sqrt - Finds the natural square root of the number
- * @n: The number to find the square root of
- * @i: Root to be tester
- * Return: sqrt
- */
-int _sqrt(int n, int i)
-{
-	if (n < 0)
+	if (rCheck(i, n) == n && n != 1)
 		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+	return (rCheck(i, n));
+
 }
