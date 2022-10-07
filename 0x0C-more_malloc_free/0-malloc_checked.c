694369442d6e0da93053660e27e79a794ccdcbf3
Codola <111078913+Codola@users.noreply.github.com>
2022-10-07 04:26:48 +0100

    Function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *m;
+
+	m = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (t);
+}
