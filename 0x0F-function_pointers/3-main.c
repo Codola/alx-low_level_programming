a418474d1371b0668200017db05f4badc7f38bb1
Codola <111078913+Codola@users.noreply.github.com>
2022-10-13 01:48:44 +0100

    Program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,31 @@
-#include "3-calc.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * main - a basic calculator
- * @argc: the size of the argument vector
- * @argv: the argument vector
- *
- * Description: This program accepts a number, an operator and another number,
- * in that order. It performs the specified operation and prints the result to
- * standard output (followed by a newline). If called with the wrong number of
- * arguments, it prints "Error" and terminates the process with a status of 98.
- * If the operator given is not one of '+', '-', '*', '/' or '%', it prints
- * "Error" and terminates the process with a status of 99. If asked to perform
- * division by 0 (e.g. using the '/' or '%' operators), it prints "Error" and
- * terminates the process with a status value of 100.
- *
- * Return: Always 0
+ * main - main function
+ * @argc: arguments
+ * @argv: array of args
+ * Return: return something
  */
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
-
-	if (argc != 4)
+	if (argc == 4)
 	{
-		puts("Error");
-		exit(98);
+		int a;
+		int b;
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
 	}
-
-	f = get_op_func(argv[2]);
-
-	if (!f)
-	{
-		puts("Error");
-		exit(99);
-	}
-
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
-
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
